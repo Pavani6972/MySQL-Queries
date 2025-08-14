17d01957aa7fe15db649121917b7867d96d5f615
Pavani6972 <22kq1a4714@pace.ac.in>
2025-08-14 22:54:39 +0530

    Add files via upload

---
+++ MySQL Queries.sql
@@ -0,0 +1,22 @@
+use work;
+create table Department(DeptID INT PRIMARY KEY,DeptName VARCHAR(20)NOT NULL);
+CREATE TABLE Employees(EmpID INT PRIMARY KEY,EmpName VARCHAR(20)NOT NULL,DeptID INT,Salary INT,HireData DATE NOT NULL,FOREIGN KEY(DeptID)REFERENCES Department(DeptID));
+INSERT INTO Department VALUES(1,'HR'),(2,'IT'),(3,'Sales');
+INSERT INTO Employees VALUES(101,'John',1,50000,'2018-02-12');
+INSERT INTO Employees VALUES(102,'Alice',2,60000,'2019-07-10'),(103,'Bob',1,55000,'2020-05-05'),(104,'Carol',3,45000,'2017-09-20');
+SELECT * FROM Employees;
+SELECT EmpName,Salary FROM Employees ORDER BY EmpName;
+SELECT e.EmpID, e.EmpName, e.Salary, e.HireData, d.DeptName  FROM Employees AS e INNER JOIN department AS d ON e.DeptID = d.DeptID  WHERE d.DeptName = 'IT';
+SELECT EmpID,EmpName,Salary,HireData FROM Employees WHERE Salary > 50000;
+SELECT EmpID,EmpName,Salary,HireData FROM Employees WHERE HireData < '2020-01-01';
+SELECT * FROM Employees ORDER BY Salary DESC;
+SELECT COUNT(*) FROM Employees;
+SELECT AVG(Salary) AS AVG_Salary FROM Employees;
+SELECT DeptID, MAX(Salary) AS MaxSalary  FROM Employees GROUP BY DeptID;
+SELECT DeptID, COUNT(*) AS EmployeeCount FROM Employees GROUP BY DeptID HAVING COUNT(*) > 1;
+SELECT * FROM Employees WHERE EmpName LIKE 'A%';
+SELECT * FROM Employees WHERE Salary BETWEEN 45000 AND 60000;
+SELECT e.EmpID,e.EmpName,e.Salary,d.DeptName FROM Employees AS e INNER JOIN Department AS d ON e.DeptID = d.DeptID WHERE e.Salary BETWEEN 45000 AND 60000;
+SELECT d.DeptName,COUNT(e.EmpID) AS NumEmployees FROM Employees AS e INNER JOIN Department AS d ON e.DeptID = d.DeptID GROUP BY d.DeptName;
+SELECT e.EmpID,e.EmpName,e.Salary,d.DeptName FROM Employees AS e LEFT JOIN Department AS d ON e.DeptID = d.DeptID;
+
